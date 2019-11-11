e1d0febc4f9b3ba0533a142bab6bb2bf845c877e
williamrosen <william.rosenberg@live.se>
2019-11-11 14:36:50 +0100

    Added files for B1 (Luhn) and B2 (Micromint) Both working according to instruction and tested against test quiz on moodle

---
+++ MicroMintSimulator.py
@@ -0,0 +1,46 @@
+'''
+Created on 11 Nov 2019
+
+@author: williamrosenberg
+'''
+from random import randint
+
+
+
+
+def Mint(u, k, c):
+    nbrOfBins = 2 ** u 
+    bins = [0] * nbrOfBins
+    genCoins = 0
+    iterations = 0
+    
+    
+    while(genCoins < c):
+        iterations += 1
+        rand = randint(0, nbrOfBins-1)
+        bins[rand] = bins[rand] + 1
+        if bins[rand] == k: genCoins = genCoins + 1
+    
+    return iterations
+
+
+def meanMint(u,k,c, iterations):
+    x = 0
+    sum = 0
+    mean = 0
+    while(x < iterations):
+        sum += Mint(u,k,c)
+        x += 1
+        
+    mean = sum/iterations
+    return mean
+
+
+def main(): 
+    print("Mean nbr of iterations: " + str(meanMint(16, 2, 1, 10000)))
+    
+
+
+if __name__ == '__main__':
+    main()
+    pass
