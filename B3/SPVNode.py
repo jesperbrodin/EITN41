136cc2e2e6401878a9f6d88ed6f26d13919cab49
brodinjesper <vov15jbr@student.lu.se>
2019-11-14 11:48:32 +0100

    här får du något att testa william

--- B3/SPVNode.py
+++ B3/SPVNode.py
@@ -3,3 +3,33 @@ Created on 13 Nov 2019
 
 @author: jesperbrodin
 '''
+
+import hashlib
+
+
+
+def SPVNode (merkle):
+   b = bytearray.fromhex(merkle)
+   for a in b:
+       print(a)
+    
+   
+   
+   ''' hashresult = ""
+    for index in range(len(merkle)): 
+        merkle[index+1] = merkle[index+1] + merkle[index]
+        hashlib.sha1(merkle[index + 1])
+    return merkle[index + 1]
+'''
+
+def main ():
+    text = ""
+    file = open("merkle.txt") 
+    for x in file:
+        text = text + x
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
+    pass
